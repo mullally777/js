1c7f76252a51afd72410bc8f4eaf078d9056fd28
Tim <timon-69@tut.by>
2019-11-30 17:13:25 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var leftBorderWidth=1; 
+
+let persone = {
+    name: "Tim",
+    age: 25,
+    isMarried: false
+};
+
+//console.log(persone["age"]);
+
+/* let arr = ['apple.jpg', 'grey.bmp', 'blue.gz'];
+console.log(arr[3]); */
+
+let answer = +prompt("Are you ok?", "Yes");
+
+console.log(typeof(answer));
+
+console.log(4+ +"5");
